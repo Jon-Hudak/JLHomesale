9e723e01466d3cb093dceacfff24252ab3c745e9
Jon-Hudak <jon@jonhudak.dev>
2024-07-31 21:18:49 -0400

    wip: tabs section

---
+++ tailwind.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily:{
+			'serif':['Source Serif Pro','ui-serif']
+		},
+		
+		extend: {
+			colors: 
+				{
+					background: "#2C3E50",
+					'backgroundLight':"#36495B",
+					offWhite:"#969FA7",
+					accent:"#5FCF80",
+				},
+			
+		},
+	},
+	plugins: [],
+}
