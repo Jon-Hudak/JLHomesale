0b3d959cd065abbcdb40ef1347831da8623ad3dd
Jon-Hudak <jon@jonhudak.dev>
2026-09-22 01:32:13 -0400

    font swap

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,7 @@ export default defineConfig({
       provider: fontProviders.fontsource(),
       name: "Montserrat",
       cssVariable: "--font-mont",
+      display:"swap"
     },
   ],
   markdown: {
